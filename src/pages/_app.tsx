63a25831568356856f9ad7528fb4b0dafd052f54
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-07-24 14:12:23 -0300

    feat: add pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
