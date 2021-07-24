5052c7d4e6c12e0aa99e300a1ea66b3b6831ac04
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-07-24 15:11:12 -0300

    chore: add plop to generate components

---
+++ generators/plopfile.js
@@ -0,0 +1,35 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'Create a new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your component?',
+        default: 'my-component'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      }
+    ]
+  })
+}
